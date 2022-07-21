39a519e826e6da1c490cc58136a344e00be9c98a
tooffuu <thdud8866@naver.com>
2022-07-22 00:54:09 +0900

    할 일 수정 코드 완성

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mysql from "mysql2/promise";
-const app = express();
-const port = 3000;
 
+const app = express();
+app.use(express.json());
+
+const port = 3000;
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -13,11 +15,12 @@ const pool = mysql.createPool({
     queueLimit : 0,
 });
 
-app.get("/todos", async (req, res) => {
-    const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
+// app.get("/todos", async (req, res) => {
+//     const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
 
-    res.json(rows);
-})
+//     res.json(rows);
+// });
+
 app.get("/todos/:id/", async (req, res) => {
     const { id } = req.params;
 
@@ -39,10 +42,57 @@ app.get("/todos/:id/", async (req, res) => {
     res.json(rows[0]);
 });
 
+app.patch("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    const { perform_date, content } = req.body;
+
+    const [ rows ] = await pool.query (
+        `
+        SELECT *
+        FROM todo
+        WHERE id = ?
+        `,
+            [id]
+    );
+        if (rows.length === 0) {
+            res.status(404).json({
+                msg : "not found",
+            });
+        }
+
+        if (!perform_date) {
+            res.status(400).json({
+                msg : "perform_date required",
+            });
+            return;
+        }
+
+        if (!content) {
+            res.status(400).json({
+                msg : "content required",
+            });
+            return;
+        }
+
+        const [rs] = await pool.query(
+            `
+            UPDATE todo
+            SET perform_date = ?, content = ?
+            WHERE id = ?
+            `,
+             [perform_date, content, id]
+        );
+
+        res.json({
+            msg : `${id}번 할 일이 수정되었습니다.`,
+        });
+});
+
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
-// })
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
